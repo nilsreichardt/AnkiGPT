58b5b847b4e12eded464c1acca29067868506822
nilsreichardt <me@nils.re>
2023-05-24 09:20:43 +0200

    Basic Flutter App template

--- bin/ankigpt.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ankigpt/ankigpt.dart' as ankigpt;
-
-void main(List<String> arguments) {
-  ankigpt.generateMoreAnkiCards();
-}
--- lib/src/parse_cards_from_csv.dart
+++
@@ -1,7 +0,0 @@
-import 'package:ankigpt/src/card.dart';
-import 'package:fast_csv/fast_csv.dart' as fast_csv;
-
-List<Card> parseCardsFromCsv(String file) {
-  final csvLines = fast_csv.parse(file);
-  return csvLines.map((line) => Card(line[0], line[1])).toList();
-}

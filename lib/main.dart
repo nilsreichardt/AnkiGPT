5861de3b3d3927c0d108cbaf25ad20705d6501d2
nilsreichardt <me@nils.re>
2023-05-26 23:07:14 +0200

    Add logo to `AppBar`

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -78,6 +79,12 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(12),
+          child: SvgPicture.asset(
+            'assets/logo/raw_logo.svg',
+          ),
+        ),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('AnkiGPT'),
         actions: const [

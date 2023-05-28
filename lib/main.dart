fff3749456c288a7a0de94db21ab0d4daa43c8e4
nilsreichardt <me@nils.re>
2023-05-29 01:58:28 +0200

    Move the footer to the scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -65,20 +65,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF00FF7F)),
         useMaterial3: true,
       ),
-      builder: (context, child) {
-        if (child == null) return const SizedBox();
-
-        const releaseDate = String.fromEnvironment(
-          'RELEASE_DATE',
-          defaultValue: 'Jan 1 2023',
-        );
-        return Column(
-          children: [
-            Expanded(child: child),
-            const Footer(releaseDate: releaseDate),
-          ],
-        );
-      },
       home: const MyHomePage(),
       routes: {
         '/imprint': (context) => const ImprintPage(),
@@ -106,6 +92,7 @@ class MyHomePage extends StatelessWidget {
           OthersOptions(),
         ],
       ),
+      bottomNavigationBar: const Footer(),
       body: const SingleChildScrollView(
         child: MaxWidthConstrainedBox(
           child: Padding(

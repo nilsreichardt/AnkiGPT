e592e1bf6eaaa42f9c5798d2e9a6b1e33abcd401
nilsreichardt <me@nils.re>
2023-05-28 02:30:49 +0200

    Support new sizes: 5, 10, 20, 50, 75 and 100

--- lib/src/providers/card_generation_size_provider.dart
+++ lib/src/providers/card_generation_size_provider.dart
@@ -2,4 +2,4 @@ import 'package:ankigpt/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final cardGenrationSizeProvider =
-    StateProvider<CardGenrationSize>((ref) => CardGenrationSize.six);
+    StateProvider<CardGenrationSize>((ref) => CardGenrationSize.five);

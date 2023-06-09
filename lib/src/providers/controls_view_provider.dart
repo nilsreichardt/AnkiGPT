ff6c65feae042a8f1db9bd63a58a52ce982806d6
nilsreichardt <me@nils.re>
2023-06-09 16:11:26 +0200

    Migrate controls provider

--- lib/src/providers/controls_view_provider.dart
+++ lib/src/providers/controls_view_provider.dart
@@ -1,34 +1,35 @@
 import 'package:ankigpt/src/models/controls_view.dart';
 import 'package:ankigpt/src/models/generate_state.dart';
 import 'package:ankigpt/src/providers/generate_provider.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final controlsViewProvider = Provider<ControlsView>(
-  (ref) {
-    final generateState = ref.watch(generateStateProvider);
-    return generateState.maybeMap<ControlsView>(
-      success: (_) => const ControlsView(
-        isGeneratedButtonEnabled: true,
-        isGenerating: false,
-        isDownloadAvailable: true,
-        isDownloadButtonVisible: true,
-        showLoadingIndicator: false,
-      ),
-      loading: (_) => ControlsView(
-        isGeneratedButtonEnabled: false,
-        isGenerating: true,
-        isDownloadAvailable: false,
-        isDownloadButtonVisible: true,
-        showLoadingIndicator: generateState is GenerationStateLoading &&
-            generateState.alreadyGeneratedCards.isNotEmpty,
-      ),
-      orElse: () => const ControlsView(
-        isGeneratedButtonEnabled: true,
-        isGenerating: false,
-        isDownloadAvailable: false,
-        isDownloadButtonVisible: false,
-        showLoadingIndicator: false,
-      ),
-    );
-  },
-);
+part 'controls_view_provider.g.dart';
+
+@riverpod
+ControlsView controlsView(ControlsViewRef ref) {
+  final generateState = ref.watch(generateNotifierProvider);
+  return generateState.maybeMap<ControlsView>(
+    success: (_) => const ControlsView(
+      isGeneratedButtonEnabled: true,
+      isGenerating: false,
+      isDownloadAvailable: true,
+      isDownloadButtonVisible: true,
+      showLoadingIndicator: false,
+    ),
+    loading: (_) => ControlsView(
+      isGeneratedButtonEnabled: false,
+      isGenerating: true,
+      isDownloadAvailable: false,
+      isDownloadButtonVisible: true,
+      showLoadingIndicator: generateState is GenerationStateLoading &&
+          generateState.alreadyGeneratedCards.isNotEmpty,
+    ),
+    orElse: () => const ControlsView(
+      isGeneratedButtonEnabled: true,
+      isGenerating: false,
+      isDownloadAvailable: false,
+      isDownloadButtonVisible: false,
+      showLoadingIndicator: false,
+    ),
+  );
+}
---
+++ lib/src/providers/controls_view_provider.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'controls_view_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$controlsViewHash() => r'436a2b450d18d9549eb12154b90d19a854f98825';
+
+/// See also [controlsView].
+@ProviderFor(controlsView)
+final controlsViewProvider = AutoDisposeProvider<ControlsView>.internal(
+  controlsView,
+  name: r'controlsViewProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$controlsViewHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ControlsViewRef = AutoDisposeProviderRef<ControlsView>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions

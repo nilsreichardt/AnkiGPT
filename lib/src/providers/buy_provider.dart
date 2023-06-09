02fe843b1dcd7a855fe3ab32a0c15efcf7b66207
nilsreichardt <me@nils.re>
2023-06-09 15:56:57 +0200

    Migrate `buyProvider`

--- lib/src/providers/buy_provider.dart
+++ lib/src/providers/buy_provider.dart
@@ -1,11 +1,14 @@
 import 'package:ankigpt/src/providers/functions_provider.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'buy_provider.g.dart';
 
 const routeFunctionsUrl = 'https://route-haaaagicoa-ew.a.run.app';
 
-final buyProvider = FutureProvider((ref) {
-  final cloudFunctions = ref.read(functionsProvider);
-  return cloudFunctions.httpsCallableFromUrl(routeFunctionsUrl).call({
+@riverpod
+Future<void> buy(BuyRef ref) async {
+  final cloudFunctions = ref.read(cloudFunctionsProvider);
+  await cloudFunctions.httpsCallableFromUrl(routeFunctionsUrl).call({
     'destination': 'analyticsBuy',
   });
-});
+}
---
+++ lib/src/providers/buy_provider.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'buy_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$buyHash() => r'927a87c5e05585b31a381c6489f41ff2ae7d3293';
+
+/// See also [buy].
+@ProviderFor(buy)
+final buyProvider = AutoDisposeFutureProvider<void>.internal(
+  buy,
+  name: r'buyProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$buyHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef BuyRef = AutoDisposeFutureProviderRef<void>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions

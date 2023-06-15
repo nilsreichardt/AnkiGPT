e81a0fc922b42e0a649a922e7bdde1cccbb06d60
nilsreichardt <me@nils.re>
2023-06-15 12:52:13 +0200

    Improve support for languages

--- lib/src/models/language.dart
+++ lib/src/models/language.dart
@@ -11,95 +11,136 @@ enum Language {
   ar, // Arabic
   hy, // Armenian
   as, // Assamese
+  ay, // Aymara
   az, // Azerbaijani
+  bm, // Bambara
   eu, // Basque
   be, // Belarusian
   bn, // Bengali
+  bho, // Bhojpuri
   bs, // Bosnian
   bg, // Bulgarian
-  my, // Burmese
   ca, // Catalan
-  zh, // Chinese
+  ceb, // Cebuano
+  ny, // Chichewa
+  zh, // Chinese (Simplified)
+  zhTW, // Chinese (Traditional)
+  co, // Corsican
   hr, // Croatian
   cs, // Czech
   da, // Danish
+  dv, // Divehi
+  doi, // Dogri
   nl, // Dutch
   en, // English
+  eo, // Esperanto
   et, // Estonian
-  fa, // Persian
+  ee, // Ewe
+  tl, // Filipino
   fi, // Finnish
   fr, // French
+  fy, // Frisian
   gl, // Galician
+  lg, // Ganda
   ka, // Georgian
   de, // German
   el, // Greek
+  gn, // Guarani
   gu, // Gujarati
   ht, // Haitian Creole
-  he, // Hebrew
+  ha, // Hausa
+  haw, // Hawaiian
+  iw, // Hebrew
   hi, // Hindi
+  hmn, // Hmong
   hu, // Hungarian
-  // is, // Icelandic, reserved keyword in Dart
+  is_, // Icelandic
+  ig, // Igbo
+  ilo, // Iloko
   id, // Indonesian
-  ga, // Irish
+  ga, // Irish Gaelic
   it, // Italian
   ja, // Japanese
+  jw, // Javanese
   kn, // Kannada
   kk, // Kazakh
   km, // Khmer
+  rw, // Kinyarwanda
+  gom, // Konkani
   ko, // Korean
-  ku, // Kurdish
+  kri, // Krio
+  ku, // Kurdish (Kurmanji)
+  ckb, // Kurdish (Sorani)
   ky, // Kyrgyz
   lo, // Lao
+  la, // Latin
   lv, // Latvian
+  ln, // Lingala
   lt, // Lithuanian
   lb, // Luxembourgish
   mk, // Macedonian
+  mai, // Maithili
   mg, // Malagasy
   ms, // Malay
   ml, // Malayalam
   mt, // Maltese
+  mi, // Maori
   mr, // Marathi
+  mniMtei, // Meiteilon (Manipuri)
+  lus, // Mizo
   mn, // Mongolian
+  my, // Myanmar (Burmese)
   ne, // Nepali
+  nso, // Northern Sotho
   no, // Norwegian
-  oc, // Occitan
-  or, // Odia
+  or, // Odia (Oriya)
+  om, // Oromo
   ps, // Pashto
+  fa, // Persian
   pl, // Polish
   pt, // Portuguese
   pa, // Punjabi
+  qu, // Quechua
   ro, // Romanian
   ru, // Russian
   sm, // Samoan
-  gd, // Scottish Gaelic
+  sa, // Sanskrit
+  gd, // Scots Gaelic
   sr, // Serbian
+  st, // Sesotho
+  sn, // Shona
   sd, // Sindhi
   si, // Sinhala
   sk, // Slovak
   sl, // Slovenian
   so, // Somali
-  st, // Southern Sotho
   es, // Spanish
+  su, // Sundanese
   sw, // Swahili
   sv, // Swedish
-  tl, // Tagalog
   tg, // Tajik
   ta, // Tamil
+  tt, // Tatar
   te, // Telugu
   th, // Thai
+  ti, // Tigrinya
+  ts, // Tsonga
   tr, // Turkish
   tk, // Turkmen
+  ak, // Twi
   uk, // Ukrainian
   ur, // Urdu
   ug, // Uyghur
   uz, // Uzbek
   vi, // Vietnamese
   cy, // Welsh
-  fy, // Western Frisian
   xh, // Xhosa
   yi, // Yiddish
   yo, // Yoruba
   zu, // Zulu
+  he, // Hebrew
+  jv, // Javanese
+  zhCN, // Chinese (Simplified)
   unknown; // Unknown
 
   String getDisplayName() {
@@ -116,92 +157,144 @@ enum Language {
         return 'Armenian';
       case Language.as:
         return 'Assamese';
+      case Language.ay:
+        return 'Aymara';
       case Language.az:
         return 'Azerbaijani';
+      case Language.bm:
+        return 'Bambara';
       case Language.eu:
         return 'Basque';
       case Language.be:
         return 'Belarusian';
       case Language.bn:
         return 'Bengali';
+      case Language.bho:
+        return 'Bhojpuri';
       case Language.bs:
         return 'Bosnian';
       case Language.bg:
         return 'Bulgarian';
-      case Language.my:
-        return 'Burmese';
       case Language.ca:
         return 'Catalan';
+      case Language.ceb:
+        return 'Cebuano';
+      case Language.ny:
+        return 'Chichewa';
       case Language.zh:
-        return 'Chinese';
+        return 'Chinese (Simplified)';
+      case Language.zhTW:
+        return 'Chinese (Traditional)';
+      case Language.co:
+        return 'Corsican';
       case Language.hr:
         return 'Croatian';
       case Language.cs:
         return 'Czech';
       case Language.da:
         return 'Danish';
+      case Language.dv:
+        return 'Divehi';
+      case Language.doi:
+        return 'Dogri';
       case Language.nl:
         return 'Dutch';
       case Language.en:
         return 'English';
+      case Language.eo:
+        return 'Esperanto';
       case Language.et:
         return 'Estonian';
-      case Language.fa:
-        return 'Persian';
+      case Language.ee:
+        return 'Ewe';
+      case Language.tl:
+        return 'Filipino';
       case Language.fi:
         return 'Finnish';
       case Language.fr:
         return 'French';
+      case Language.fy:
+        return 'Frisian';
       case Language.gl:
         return 'Galician';
+      case Language.lg:
+        return 'Ganda';
       case Language.ka:
         return 'Georgian';
       case Language.de:
         return 'German';
       case Language.el:
         return 'Greek';
+      case Language.gn:
+        return 'Guarani';
       case Language.gu:
         return 'Gujarati';
       case Language.ht:
         return 'Haitian Creole';
-      case Language.he:
+      case Language.ha:
+        return 'Hausa';
+      case Language.haw:
+        return 'Hawaiian';
+      case Language.iw:
         return 'Hebrew';
       case Language.hi:
         return 'Hindi';
+      case Language.hmn:
+        return 'Hmong';
       case Language.hu:
         return 'Hungarian';
-      // case Language.is: // is, reserved keyword in Dart
-      //   return 'Icelandic';
+      case Language.is_:
+        return 'Icelandic';
+      case Language.ig:
+        return 'Igbo';
+      case Language.ilo:
+        return 'Iloko';
       case Language.id:
         return 'Indonesian';
       case Language.ga:
-        return 'Irish';
+        return 'Irish Gaelic';
       case Language.it:
         return 'Italian';
       case Language.ja:
         return 'Japanese';
+      case Language.jw:
+        return 'Javanese';
       case Language.kn:
         return 'Kannada';
       case Language.kk:
         return 'Kazakh';
       case Language.km:
         return 'Khmer';
+      case Language.rw:
+        return 'Kinyarwanda';
+      case Language.gom:
+        return 'Konkani';
       case Language.ko:
         return 'Korean';
+      case Language.kri:
+        return 'Krio';
       case Language.ku:
-        return 'Kurdish';
+        return 'Kurdish (Kurmanji)';
+      case Language.ckb:
+        return 'Kurdish (Sorani)';
       case Language.ky:
         return 'Kyrgyz';
       case Language.lo:
         return 'Lao';
+      case Language.la:
+        return 'Latin';
       case Language.lv:
         return 'Latvian';
+      case Language.ln:
+        return 'Lingala';
       case Language.lt:
         return 'Lithuanian';
       case Language.lb:
         return 'Luxembourgish';
       case Language.mk:
         return 'Macedonian';
+      case Language.mai:
+        return 'Maithili';
       case Language.mg:
         return 'Malagasy';
       case Language.ms:
@@ -210,36 +303,56 @@ enum Language {
         return 'Malayalam';
       case Language.mt:
         return 'Maltese';
+      case Language.mi:
+        return 'Maori';
       case Language.mr:
         return 'Marathi';
+      case Language.mniMtei:
+        return 'Meiteilon (Manipuri)';
+      case Language.lus:
+        return 'Mizo';
       case Language.mn:
         return 'Mongolian';
+      case Language.my:
+        return 'Myanmar (Burmese)';
       case Language.ne:
         return 'Nepali';
+      case Language.nso:
+        return 'Northern Sotho';
       case Language.no:
         return 'Norwegian';
-      case Language.oc:
-        return 'Occitan';
       case Language.or:
-        return 'Odia';
+        return 'Odia (Oriya)';
+      case Language.om:
+        return 'Oromo';
       case Language.ps:
         return 'Pashto';
+      case Language.fa:
+        return 'Persian';
       case Language.pl:
         return 'Polish';
       case Language.pt:
         return 'Portuguese';
       case Language.pa:
         return 'Punjabi';
+      case Language.qu:
+        return 'Quechua';
       case Language.ro:
         return 'Romanian';
       case Language.ru:
         return 'Russian';
       case Language.sm:
         return 'Samoan';
+      case Language.sa:
+        return 'Sanskrit';
       case Language.gd:
-        return 'Scottish Gaelic';
+        return 'Scots Gaelic';
       case Language.sr:
         return 'Serbian';
+      case Language.st:
+        return 'Sesotho';
+      case Language.sn:
+        return 'Shona';
       case Language.sd:
         return 'Sindhi';
       case Language.si:
@@ -250,28 +363,34 @@ enum Language {
         return 'Slovenian';
       case Language.so:
         return 'Somali';
-      case Language.st:
-        return 'Southern Sotho';
       case Language.es:
         return 'Spanish';
+      case Language.su:
+        return 'Sundanese';
       case Language.sw:
         return 'Swahili';
       case Language.sv:
         return 'Swedish';
-      case Language.tl:
-        return 'Tagalog';
       case Language.tg:
         return 'Tajik';
       case Language.ta:
         return 'Tamil';
+      case Language.tt:
+        return 'Tatar';
       case Language.te:
         return 'Telugu';
       case Language.th:
         return 'Thai';
+      case Language.ti:
+        return 'Tigrinya';
+      case Language.ts:
+        return 'Tsonga';
       case Language.tr:
         return 'Turkish';
       case Language.tk:
         return 'Turkmen';
+      case Language.ak:
+        return 'Twi';
       case Language.uk:
         return 'Ukrainian';
       case Language.ur:
@@ -284,8 +403,6 @@ enum Language {
         return 'Vietnamese';
       case Language.cy:
         return 'Welsh';
-      case Language.fy:
-        return 'Western Frisian';
       case Language.xh:
         return 'Xhosa';
       case Language.yi:
@@ -294,6 +411,12 @@ enum Language {
         return 'Yoruba';
       case Language.zu:
         return 'Zulu';
+      case Language.he:
+        return 'Hebrew';
+      case Language.jv:
+        return 'Javanese';
+      case Language.zhCN:
+        return 'Chinese (Simplified)';
       case Language.unknown:
         return 'Unknown';
     }

6a9f28d18e848f2af139975a7844e0f5a987bb0f
nilsreichardt <me@nils.re>
2023-06-20 20:41:20 +0200

    Add `DeleteCardController`

--- lib/src/providers/delete_card_provider.dart
+++ lib/src/providers/delete_card_provider.dart
@@ -1,5 +1,8 @@
+import 'package:ankigpt/src/models/anki_card.dart';
 import 'package:ankigpt/src/models/card_id.dart';
 import 'package:ankigpt/src/models/session_id.dart';
+import 'package:ankigpt/src/providers/cards_list_provider.dart';
+import 'package:ankigpt/src/providers/logger/logger_provider.dart';
 import 'package:ankigpt/src/providers/session_repository_provider.dart';
 import 'package:queue/queue.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -11,6 +14,66 @@ part 'delete_card_provider.g.dart';
 /// The [Queue] is used to avoid race conditions when deleting cards.
 @Riverpod(keepAlive: true)
 Queue deleteQueue(DeleteQueueRef ref) => Queue();
+
+@Riverpod(keepAlive: true)
+class DeleteCardController extends _$DeleteCardController {
+  final _queue = Queue();
+  AnkiCard? _lastDeletedCard;
+
+  @override
+  void build() {
+    return;
+  }
+
+  void delete({
+    required CardId cardId,
+    required SessionId sessionId,
+  }) {
+    // Remove card locally first to avoid loading states.
+    _lastDeletedCard = ref.read(cardsListProvider.notifier).get(cardId);
+    ref.read(cardsListProvider.notifier).remove(cardId);
+
+    _queue.add(
+      () async {
+        final repository = ref.read(sessionRepositoryProvider);
+        ref.read(loggerProvider).v('Delete card "$cardId"');
+        await repository.deleteCard(
+          sessionId: sessionId,
+          cardId: cardId,
+        );
+      },
+    );
+  }
+
+  bool undo({
+    required SessionId sessionId,
+  }) {
+    if (_lastDeletedCard == null) {
+      ref.read(loggerProvider).w(
+          'Could not undo card deletion because there has been no card deleted before');
+      return false;
+    }
+
+    final id = _lastDeletedCard!.id;
+    if (_queue.contains(id)) {
+      // To prevent unnessesary calls to the database, we remove the call to
+      // delete, if the call hasn't made yet.
+      _queue.remove(id);
+    } else {
+      _queue.add(() async {
+        final repository = ref.read(sessionRepositoryProvider);
+        ref.read(loggerProvider).v('Undo delete card "$id"');
+        await repository.undoDeleteCard(
+          sessionId: sessionId,
+          cardId: id,
+        );
+      });
+    }
+
+    ref.read(cardsListProvider.notifier).add(_lastDeletedCard!);
+    return true;
+  }
+}
 
 @riverpod
 void deleteCard(
--- lib/src/providers/delete_card_provider.g.dart
+++ lib/src/providers/delete_card_provider.g.dart
@@ -227,4 +227,22 @@ class UndoDeleteCardProvider extends AutoDisposeProvider<void> {
     return _SystemHash.finish(hash);
   }
 }
+
+String _$deleteCardControllerHash() =>
+    r'4200217518f4cdadf8017cbd72d3a93bd1c74819';
+
+/// See also [DeleteCardController].
+@ProviderFor(DeleteCardController)
+final deleteCardControllerProvider =
+    NotifierProvider<DeleteCardController, void>.internal(
+  DeleteCardController.new,
+  name: r'deleteCardControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$deleteCardControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DeleteCardController = Notifier<void>;
 // ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions

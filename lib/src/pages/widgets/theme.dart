7ff3b07fefd45e78ccf442b7b58b60f7bbbb39be
nilsreichardt <me@nils.re>
2023-07-03 15:58:11 +0200

    Add font "Ubuntu"

--- lib/src/pages/widgets/theme.dart
+++ lib/src/pages/widgets/theme.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
-final _colorScheme = ColorScheme.fromSeed(seedColor: const Color(0xFF00FF7F));
+final _colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color(0xFF00FF7F),
+);
+const darkGreen = Color(0xFF001D0E);
+
 final ankigptTheme = ThemeData(
   colorScheme: _colorScheme,
   useMaterial3: true,
@@ -11,6 +15,12 @@ final ankigptTheme = ThemeData(
   snackBarTheme: const SnackBarThemeData(
     behavior: SnackBarBehavior.floating,
   ),
+  fontFamily: 'Ubuntu',
+  textTheme: ThemeData.light().textTheme.apply(
+        bodyColor: darkGreen,
+        displayColor: darkGreen,
+      ),
+  scaffoldBackgroundColor: const Color(0xFFE9FFF4),
   pageTransitionsTheme: PageTransitionsTheme(
     builders: TargetPlatform.values.asMap().map(
           (key, value) => MapEntry(

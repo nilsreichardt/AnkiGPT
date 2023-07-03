c9597cc50076ea0f6edd2e31b591b59d49ec4b56
nilsreichardt <me@nils.re>
2023-07-03 16:39:35 +0200

    Add upload file button

--- lib/src/pages/home_page/input_section.dart
+++ lib/src/pages/home_page/input_section.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
+import 'package:ankigpt/main.dart';
 import 'package:ankigpt/src/pages/widgets/max_width_constrained_box.dart';
+import 'package:ankigpt/src/providers/generate_provider.dart';
+import 'package:ankigpt/src/providers/has_plus_provider.dart';
 import 'package:ankigpt/src/providers/slide_text_field_controller_provider.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
@@ -17,8 +20,10 @@ class InputSection extends StatelessWidget {
         children: [
           SizedBox(height: min(65, MediaQuery.of(context).size.width * 0.1)),
           const _Headline(),
-          const SizedBox(height: 50),
+          const SizedBox(height: 38),
           const _InputField(),
+          const SizedBox(height: 12),
+          const _UploadFileButton(),
         ],
       ),
     );
@@ -71,6 +76,51 @@ Supports all languages.''',
       minLines: 6,
       maxLines: null,
       keyboardType: TextInputType.multiline,
+    );
+  }
+}
+
+class _UploadFileButton extends ConsumerWidget {
+  const _UploadFileButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    const borderRadius = BorderRadius.all(Radius.circular(12));
+    final hasPlus = ref.watch(hasPlusProvider);
+    return InkWell(
+      borderRadius: borderRadius,
+      onTap: () {
+        final hasPlus = ref.read(hasPlusProvider);
+        if (!hasPlus) {
+          showPlusDialog(context);
+          return;
+        }
+
+        ref.read(generateNotifierProvider.notifier).pickFile();
+      },
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 275),
+        child: Material(
+          key: ValueKey(hasPlus),
+          borderRadius: borderRadius,
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.15),
+          child: SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(40),
+              child: Column(
+                children: [
+                  const Icon(Icons.upload_file),
+                  SizedBox(height: hasPlus ? 13 : 0),
+                  const Text('Upload PDF file'),
+                  SizedBox(height: hasPlus ? 13 : 8),
+                  if (!hasPlus) const PlusBadge(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

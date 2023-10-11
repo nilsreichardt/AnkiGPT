7499821815fbfccca8f6681cff7ac254dd0b154b
nilsreichardt <me@nils.re>
2023-10-11 17:16:07 +0200

    Add `currentMonthUsageProvider`

---
+++ lib/src/providers/current_month_usage_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'current_month_usage_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$currentMonthUsageHash() => r'47238d9b6f80eb1613f7a97ec44e9b36cf3bfc50';
+
+/// See also [currentMonthUsage].
+@ProviderFor(currentMonthUsage)
+final currentMonthUsageProvider = AutoDisposeProvider<int>.internal(
+  currentMonthUsage,
+  name: r'currentMonthUsageProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$currentMonthUsageHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CurrentMonthUsageRef = AutoDisposeProviderRef<int>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions

60194e26b438fbde5cb9575920cb421c769f2522
nilsreichardt <me@nils.re>
2023-10-11 15:26:48 +0200

    Add `usage` attribute to `AppUser`

--- lib/src/models/app_user.dart
+++ lib/src/models/app_user.dart
@@ -7,8 +7,18 @@ part 'app_user.g.dart';
 class AppUser with _$AppUser {
   const factory AppUser({
     @Default(false) bool hasPlus,
+    @Default(Usage(generatedCardsCurrentMonth: 0)) Usage usage,
   }) = _AppUser;
 
   factory AppUser.fromJson(Map<String, dynamic> json) =>
       _$AppUserFromJson(json);
+}
+
+@freezed
+class Usage with _$Usage {
+  const factory Usage({
+    @Default(0) int generatedCardsCurrentMonth,
+  }) = _Usage;
+
+  factory Usage.fromJson(Map<String, dynamic> json) => _$UsageFromJson(json);
 }

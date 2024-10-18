a6e8ed193a7f8f79fff053b0769aeeae3c2f7dcb
nilsreichardt <me@nils.re>
2024-10-18 15:15:04 +0200

    Revert "Revert "Redesign home page header (#264)" (#266)"
    This reverts commit 4fd3a6ef825aa43ae58d97f4b8a91446936a9f24.

--- lib/src/providers/traction_provider.g.dart
+++ lib/src/providers/traction_provider.g.dart
@@ -25,5 +25,21 @@ final getTractionStreamProvider = StreamProvider<String>.internal(
 );
 
 typedef GetTractionStreamRef = StreamProviderRef<String>;
+String _$getTractionStreamIntHash() =>
+    r'e9d414fbec59791cdfb12d826f4350cf88cd2a05';
+
+/// See also [getTractionStreamInt].
+@ProviderFor(getTractionStreamInt)
+final getTractionStreamIntProvider = StreamProvider<int>.internal(
+  getTractionStreamInt,
+  name: r'getTractionStreamIntProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getTractionStreamIntHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetTractionStreamIntRef = StreamProviderRef<int>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
